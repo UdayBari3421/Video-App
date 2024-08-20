09c91a1561f6947e95d39ddf741ddd0d72f41cd8
UdayBari3421 <udaybari2003@gmail.com>
2024-08-20 14:21:40 +0530

    refreshToken refresh change done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUser,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -25,6 +26,8 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser);
 
+// secure route
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
